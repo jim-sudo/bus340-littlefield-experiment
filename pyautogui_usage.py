e3b935bcc32cebeaabacc4c1950af435ce1c0800
Jim <jimmyosburn@rocketmail.com>
2025-11-20 00:29:25 -0800

    Added pyautogui, but noticed bug where last 3 days of activity isnt seen on charts

---
+++ pyautogui_usage.py
@@ -0,0 +1,20 @@
+import pyautogui as gui
+import time
+import os
+
+
+
+# firefox location: (34, 68)
+# littlefield bookmark: (298, 134)
+# Data button: (197, 642)
+
+def main():
+   print('Let\'s do this!')
+   gui.click(34, 68)
+   time.sleep(30)
+   gui.click(298, 134)
+   time.sleep(30)
+   gui.click(187, 642)
+   time.sleep(15)
+   gui.hotkey('alt', 'f4')
+
